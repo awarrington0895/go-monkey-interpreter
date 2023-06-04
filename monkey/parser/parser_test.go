c18e55737feab9f0aee380c51e25d25d151f6d2a
Alex Warrington <awarrington0895@gmail.com>
2023-06-04 13:06:46 +0000

    Add string representations of each AST node

--- monkey/parser/parser_test.go
+++ monkey/parser/parser_test.go
@@ -5,6 +5,7 @@ import (
 
 	"monkey/ast"
 	"monkey/lexer"
+	"monkey/token"
 )
 
 func TestLetStatements(t *testing.T) {
@@ -80,6 +81,28 @@ func TestReturnStatements(test *testing.T) {
 		if returnStmt.TokenLiteral() != "return" {
 			test.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
 		}
+	}
+}
+
+func TestString(t *testing.T) {
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
 
